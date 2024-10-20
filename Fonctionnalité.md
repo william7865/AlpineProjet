9754da56f4d877649fc21338f1e89a4273734f79
William Lin <linwilliam14@gmail.com>
2024-10-20 23:10:52 +0200

    Site Marchand

---
+++ Fonctionnalité.md
@@ -0,0 +1,5 @@
+Barre de recherche pour les produits (Produits) ou pour trouver un magasin(Nos Magasins).
+Onglets pour chaque produit afin d'avoir plus de détails. 
+Ajout des produits dans le panier. 
+Incrémentation et désincrémentation pour chaque produit ajouté dans le panier. 
+Mode nuit et jour.
---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+const panierLink = document.getElementById('panierLink');
+const closeButton = document.getElementById('closeButton');
+panierLink.addEventListener('click', function(event) {
+    popup.style.display = 'flex';
+});
+closeButton.addEventListener('click', function() {
+    popup.style.display = 'none';
+});
